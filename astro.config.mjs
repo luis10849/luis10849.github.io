1654edf2e4528df6887d8fee8fdbfa54945cd6a5
luis10849 <53123277+luis10849@users.noreply.github.com>
2025-05-18 19:03:05 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luis10849.github.io',
-  base: "blog",
+  base: "luis10849.github.io",
   output:'server',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
   adapter: netlify(),
